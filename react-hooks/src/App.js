7d8ff60c3d3940f3458f98fb4b8cc637a53157e6
정우찬 <vdncks25@naver.com>
2019-06-04 14:09:57 +0900

    FEAT : Make counter using useState hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount]=useState(0);
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      {count}
+      <button onClick={() => setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count-1)}>Decrement</button>
+    </>
   );
 }
 
